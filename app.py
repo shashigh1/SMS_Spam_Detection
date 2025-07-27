68deca64eae3c4e8d524c8b2e364f01333ae0dc6
shashikant <shashi18102001@gmail.com>
2025-07-27 12:54:02 +0530

    Initial commit - SMS Spam Classifier with local punkt

--- app.py
+++ app.py
@@ -1,22 +1,58 @@
-# app.py
 import streamlit as st
 import pickle
-from preprocessing import transform_text
+import nltk
+import string
+import os
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from nltk.tokenize import word_tokenize
+
+# Point to local nltk_data folder for Streamlit Cloud
+nltk.data.path.append('./nltk_data')
 
 # Load model and vectorizer
 model = pickle.load(open('model.pkl', 'rb'))
-vectorizer = pickle.load(open('vectorizer.pkl', 'rb'))
+tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
 
-st.title("Email/SMS Spam Classifier")
+# Initialize stemmer
+ps = PorterStemmer()
+
+# Preprocessing function
+def transform_text(text):
+    text = text.lower()
+    text = word_tokenize(text)
+    
+    y = []
+    for word in text:
+        if word.isalnum():
+            y.append(word)
+    
+    text = y[:]
+    y.clear()
+
+    for word in text:
+        if word not in stopwords.words('english') and word not in string.punctuation:
+            y.append(ps.stem(word))
+    
+    return " ".join(y)
+
+# Streamlit UI
+st.title("SMS Spam Classifier")
 
 input_sms = st.text_area("Enter the message")
 
 if st.button('Predict'):
+    # Preprocess
     transformed = transform_text(input_sms)
-    vector_input = vectorizer.transform([transformed]).toarray()
+
+    # Vectorize
+    vector_input = tfidf.transform([transformed])
+
+    # Predict
     result = model.predict(vector_input)[0]
 
+    # Output
     if result == 1:
-        st.error("Spam Message")
+        st.error("Spam 😈")
     else:
-        st.success("Not Spam")
+        st.success("Not Spam 😊")
